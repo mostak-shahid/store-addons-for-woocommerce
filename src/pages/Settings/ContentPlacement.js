bd87a9c8b7879213248ee1461674a142d27f881e
Md. Mostak Shahid <mostak.shahid@gmail.com>
2026-07-04 20:49:16 +0600

    working on Cart page content placement

--- src/pages/Settings/ContentPlacement.js
+++ src/pages/Settings/ContentPlacement.js
@@ -186,15 +186,14 @@ const ContentPlacement = () => {
                                                     className={`rounded-0 p-0 border-3 w-100 ${settings?.cart?.content_placement?.layout == layoutOption ? 'active-image' : ''}`}
                                                     onChange={(e) => handleChange('cart.content_placement.layout', layoutOption)}
                                                 >
-                                                    <div className={`d-flex flex-column content-layout content-layout-${layoutOption}`} style={{ border: '1px solid #f0f0f0', padding: '10px', borderRadius: '5px', gap: '5px' }}>
+                                                    <div className={`content-layout content-layout-${layoutOption} border rounded-2 p-2`}>
                                                         <div className="img-con">
-                                                            <span className="d-inline-block" style={{ width: '40px', height: '40px', backgroundColor: '#f0f0f0' }}>
-                                                            </span>
+                                                            <span className="d-inline-block"></span>
                                                         </div>
                                                         <div className="text-con d-flex flex-column" style={{ gap: '5px' }}>
-                                                            <span className="d-inline-block" style={{ width: '100%', height: '10px', backgroundColor: '#f0f0f0' }}></span>
-                                                            <span className="d-inline-block" style={{ width: '80%', height: '10px', backgroundColor: '#f0f0f0' }}></span>
-                                                            <span className="d-inline-block" style={{ width: '60%', height: '10px', backgroundColor: '#f0f0f0' }}></span>
+                                                            <span className="d-inline-block" style={{ width: '100%' }}></span>
+                                                            <span className="d-inline-block" style={{ width: '80%' }}></span>
+                                                            <span className="d-inline-block" style={{ width: '60%' }}></span>
                                                         </div>
                                                     </div>
 
